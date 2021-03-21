aceaa6cfc00706f2003308b897eb96f05c9c9e8d
Maxim <7964277@gmail.com>
2021-03-21 10:44:04 +0300

    setted country chooser to tab zero

--- app/src/main/java/com/example/mposh/fragment/TabZero.java
+++ app/src/main/java/com/example/mposh/fragment/TabZero.java
@@ -22,9 +22,9 @@ public class TabZero extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.activity_slotmachine_fragment_one, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_country_choose, container, false);
 
-        dropdown = rootView.findViewById(R.id.spinner);
+        dropdown = rootView.findViewById(R.id.spinner_choose_country);
         chooseTown();
 
         return rootView;
